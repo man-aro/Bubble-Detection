ed06f192d9994a96ae60f5b3c03ac8b2c1a5bb44
Manish Rajkumar Arora <manish.arora@glasgow.ac.uk>
2024-11-27 15:25:58 +0000

    Add files via upload

--- BubDet.py
+++ BubDet.py
@@ -8,9 +8,17 @@ Created on Wed Nov 27 14:13:17 2024
 import streamlit as st
 import pandas as pd
 
-st.write("Bubble Detection")
 
 
-window_size = st.number_input('Window Size', min_value = 30, max_value = 180, step = 30)
+st.title('Deep Calibration Framework for Detecting Stock Bubbles using Option Prices')
+
+
+
+
+stock = st.selectbox("Select a Stock: ", ('MSFT', 'AMZN', 'NVDA', 'AMD', 'META'))
+st.write("Selected Stock: ", stock)
+
+
+window_size = st.number_input('Window Size', min_value = 30, max_value = 60, step = 30)
 st.write('Current Window Size is ', window_size)
 
