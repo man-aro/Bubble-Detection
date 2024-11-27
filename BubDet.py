33000db5fd253fa75695d29078b439e62ee8c54d
Manish Rajkumar Arora <manish.arora@glasgow.ac.uk>
2024-11-27 14:49:27 +0000

    Add files via upload

--- BubDet.py
+++ BubDet.py
@@ -8,10 +8,9 @@ Created on Wed Nov 27 14:13:17 2024
 import streamlit as st
 import pandas as pd
 
-st.write("First Attempt: Create Table")
+st.write("Bubble Detection")
 
 
-option = st.selectbox('Contact', ('Email', 'Phone'), ('marora6899@gmail.com', '+44 0 7476720446'))
-st.write('You selected:', option)
-
+window_size = st.number_input('Window Size', min_value = 30, max_value = 180, step = 30)
+st.write('Current Window Size is ', window_size)
 
