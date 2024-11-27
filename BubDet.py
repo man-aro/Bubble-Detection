fb8882c22fd1a1a18e374d0d16f458e010c69dfb
Manish Rajkumar Arora <manish.arora@glasgow.ac.uk>
2024-11-27 15:50:51 +0000

    Add files via upload

--- BubDet.py
+++ BubDet.py
@@ -15,21 +15,16 @@ st.title('Deep Calibration Framework for Detecting Stock Bubbles using Option Pr
 
 
 
-stock = st.selectbox("Select a Stock: \n Window size determines the number of historical observations (days) from which information is considered for bubble detection."
-                     , ('MSFT', 'AMZN', 'NVDA', 'AMD', 'META'))
+stock = st.selectbox("Select a Stock: ", ('MSFT', 'AMZN', 'NVDA', 'AMD', 'META'))
 st.write("Selected Stock: ", stock)
-
-
-
-st.write('')
-st.write('The selection of a window length can be interpretated as being analogous to the risk-appetite of investors. For instance, a more conservative (larger window) detection approach would signal lower regulatory pressure on capital requirements, and a riskier outlook from investors, who would reveal looser preferences towards protecting positions from the potential  presence of bubbles.')
-
 
 
 
 
 window_size = st.selectbox("Select a Window Size (days): ",('30', '60'))
+st.write("Window size determines the number of historical observations (days) from which information is considered for bubble detection.")
 st.write("Selected Window Size (days) is: ", window_size)
 
 sig_level = st.selectbox("Select a Level of Significance (%): ", ('10%', '5%', '1%'))
+st.write("Bubble detection is stricter for lower levels of significance.")
 st.write("Selected Significance Level: ", sig_level)
